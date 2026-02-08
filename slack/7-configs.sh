65b32ff669fd6666f3ff751fbada83b7ea3dc558
Eric Fernandes Ferreira <candelabrus@gmail.com>
2026-02-08 20:39:26 -0300

    slackware scripts updated

--- slack/7-configs.sh
+++ slack/7-configs.sh
@@ -4,20 +4,23 @@
 cp /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 
 # Arrumando o teclado
-cp configs/90-keyboard-layout.conf /etc/X11/xorg.conf.d/
+cp configs/90-keyboard-layout.conf /etc/X11/xorg.conf.d
 
 # Definindo driver Intel (Xe ou i915)
-cp configs/xe.conf /etc/modprobe.d/
+cp configs/xe.conf /etc/modprobe.d
 
 # Arrumando o xorg para Nvidia Prime
-cp configs/prime-run /usr/bin/
+cp configs/prime-run /usr/bin
 cp configs/80-nvidia-pm.rules /etc/udev/rules.d
-cp configs/nvidia-options.conf /etc/modprobe.d/
-cp configs/00-fix-nvidia.conf /etc/X11/xorg.conf.d/
+cp configs/nvidia-options.conf /etc/modprobe.d
+cp configs/00-fix-nvidia.conf /etc/X11/xorg.conf.d
 echo "# Inicia o NVIDIA Persistenced Daemon
 if [ -x /usr/bin/nvidia-persistenced ]; then
   /usr/bin/nvidia-persistenced --user root
 fi" >> /etc/rc.d/rc.local
+
+# Isolando da Nvidia de ser chamada em programas electrons
+cp configs/vulkan_intel.sh /etc/profile.d
 
 # Configs Extras - UDEV Rules
 cp configs/10-mouse-powersave.rules /etc/udev/rules.d
---
+++ slack/configs/vulkan_intel.sh
@@ -0,0 +1,1 @@
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json
